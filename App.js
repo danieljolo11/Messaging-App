876e0143f0c46e0dada4765b8b9f0439b0116067
Daniel Jolo <danieljolo4899@gmail.com>
2022-09-21 10:56:58 +0800

    save

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-600">Open up App.js to start working on your app!</Text>
+      <Text className="text-red-600">Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
